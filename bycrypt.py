628947616fbc8c29f03f71cce00e14261676c4e9
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-01-05 11:12:06 +0330

    Add files via upload

---
+++ bycrypt.py
@@ -0,0 +1,13 @@
+# requirement : pip install bcrypt
+import bcrypt
+# def for hashing password with bcrypt method
+def hash_bcrypt(password):
+    hash_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hash_password
+# verify password
+def verify_password(hashed, plain):
+    return bcrypt.checkpw(plain.encode(), hashed)
+password = "my_password"
+hash_password = hash_bcrypt(password)
+# now we can store password in database or to se result like here print it
+print(hash_password)
